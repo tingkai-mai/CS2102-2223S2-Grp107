ecd38c1a98833d30d8169619412b292708c568a8
ferusel <wheatgrass.mc@gmail.com>
2023-04-08 22:39:19 +0800

    hehe

--- Assignment 2/part1.sql
+++ Assignment 2/part1.sql
@@ -5,15 +5,17 @@ BEGIN
     IF EXISTS (SELECT * FROM packages P WHERE P.request_id = NEW.id) THEN
         RETURN NEW;
     ELSE
+        -- RAISE EXCEPTION 'Each delivery request must have at least one package.';
         RETURN NULL;
     END IF; 
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE CONSTRAINT TRIGGER trigger_1
-BEFORE INSERT ON delivery_requests
+AFTER INSERT ON delivery_requests
 DEFERRABLE INITIALLY IMMEDIATE
-FOR EACH ROW EXECUTE FUNCTION trigger_fn_1();
+FOR EACH ROW 
+EXECUTE FUNCTION trigger_fn_1();
 
 -- Q2 Package
 CREATE OR REPLACE FUNCTION trigger_fn_2() RETURNS TRIGGER
@@ -25,6 +27,7 @@ BEGIN
     IF largest_pkg_id + 1 = NEW.package_id THEN 
         RETURN NEW;
     ELSE
+        -- RAISE EXCEPTION 'Package ID is invalid. Package IDs must be consecutive integers starting from 1';
         RETURN NULL;
     END IF; 
 END;
@@ -44,6 +47,7 @@ BEGIN
     IF largest_pickup_id + 1 = NEW.pickup_id THEN 
         RETURN NEW;
     ELSE
+        -- RAISE EXCEPTION 'Unsuccessful pickup ID is invalid. Unsuccessful pickup IDs must be consecutive integers starting from 1';
         RETURN NULL;
     END IF; 
 END;
@@ -60,12 +64,13 @@ DECLARE
     req_submission_time TIMESTAMP;
     prev_pickup_time TIMESTAMP;
 BEGIN
+    SELECT DR.submission_time INTO req_submission_time FROM delivery_requests DR WHERE NEW.request_id = DR.id;
     -- First unsuccessful pickup
-    IF NEW.pickup_id = 1 THEN 
-        SELECT DR.submission_time INTO req_submission_time FROM delivery_requests DR WHERE NEW.request_id = DR.id;
+    IF NEW.pickup_id = 1 THEN
         IF NEW.pickup_time > req_submission_time THEN
             RETURN NEW;
-        ELSE 
+        ELSE
+            -- RAISE EXCEPTION 'Timestamp of unsuccessful pickup is invalid.';
             RETURN NULL;
         END IF;
     -- Check for previous unsuccessful pickup
@@ -74,9 +79,10 @@ BEGIN
         FROM unsuccessful_pickups U 
         WHERE U.request_id = NEW.request_id
         AND U.pickup_id = NEW.pickup_id - 1;
-        IF NEW.pickup_time > prev_pickup_time THEN
+        IF NEW.pickup_time > prev_pickup_time AND NEW.pickup_time > req_submission_time THEN
             RETURN NEW;
-        ELSE 
+        ELSE
+            -- RAISE EXCEPTION 'Timestamp of unsuccessful pickup is invalid.';
             RETURN NULL;
         END IF;
     END IF; 
@@ -97,6 +103,7 @@ BEGIN
     IF largest_leg_id + 1 = NEW.leg_id THEN 
         RETURN NEW;
     ELSE
+        -- RAISE EXCEPTION 'Leg ID is invalid. Leg IDs must be consecutive integers starting from 1';
         RETURN NULL;
     END IF; 
 END;
@@ -113,7 +120,6 @@ DECLARE
     prev_time TIMESTAMP;
     prev_leg_endtime TIMESTAMP;
 BEGIN
-    SELECT INTO largest_leg_id COALESCE(MAX(leg_id),0) FROM legs L WHERE NEW.request_id = L.request_id;
     -- Check if this is a first leg
     IF NEW.leg_id = 1 THEN
         -- Check if there exists any unsuccessful pickups
@@ -125,7 +131,8 @@ BEGIN
         -- Check if valid start time
         IF NEW.start_time > prev_time THEN
             RETURN NEW;
-        ELSE 
+        ELSE
+            -- RAISE EXCEPTION 'Start time of first leg is invalid.';
             RETURN NULL;
         END IF;
     ELSE
@@ -136,10 +143,12 @@ BEGIN
         AND L.leg_id = NEW.leg_id - 1;
 
         IF prev_leg_endtime IS NULL THEN
+            -- RAISE EXCEPTION 'End time of previous leg cannot be NULL.';
             RETURN NULL;
-        ELSE IF NEW.start_time > prev_leg_endtime THEN
+        ELSIF NEW.start_time > prev_leg_endtime THEN
             RETURN NEW;
         ELSE
+            -- RAISE EXCEPTION 'Start time of leg is invalid.';
             RETURN NULL;
         END IF;
     END IF; 
@@ -149,3 +158,155 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER trigger_6
 BEFORE INSERT ON legs
 FOR EACH ROW EXECUTE FUNCTION trigger_fn_6();
+
+-- Q8 Unsuccessful deliveries
+CREATE OR REPLACE FUNCTION trigger_fn_8() RETURNS TRIGGER
+AS $$ 
+DECLARE 
+    leg_start_time TIMESTAMP;
+BEGIN
+    SELECT start_time INTO leg_start_time FROM Legs L WHERE L.leg_id = NEW.leg_id AND NEW.request_id = L.request_id;
+    
+    IF NEW.attempt_time > leg_start_time THEN
+        -- RAISE EXCEPTION 'The timestamp of each unsuccessful_delivery must be after the start_time of the corresponding leg.';
+        RETURN NULL;
+    ELSE 
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_8
+BEFORE INSERT ON unsuccessful_deliveries
+FOR EACH ROW EXECUTE FUNCTION trigger_fn_8();
+
+-- Q9 For each delivery request, there can only be at most three unsuccessful deliveries
+CREATE OR REPLACE FUNCTION trigger_fn_9() RETURNS TRIGGER
+AS $$
+DECLARE
+    unsuccessful_deliveries_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO unsuccessful_deliveries_count FROM unsuccessful_deliveries u WHERE NEW.request_id = u.request_id;
+
+    IF (unsuccessful_deliveries_count >= 3) THEN
+        -- RAISE EXCEPTION 'Cannot have more than three unsuccessful deliveries in a delivery request!';
+        RETURN NULL;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_9
+BEFORE INSERT ON unsuccessful_deliveries
+FOR EACH ROW EXECUTE FUNCTION trigger_fn_9();
+
+
+-- Q10 Cancelled requests
+CREATE OR REPLACE FUNCTION trigger_fn_10() RETURNS TRIGGER
+AS $$
+DECLARE 
+    delivery_req_submission_time TIMESTAMP;
+BEGIN
+    SELECT D.submission_time INTO delivery_req_submission_time FROM delivery_requests D WHERE D.id = NEW.id; 
+    IF NEW.cancel_time <= delivery_req_submission_time THEN 
+        -- RAISE EXCEPTION 'The cancel_time of a cancelled request must be after the submission_time of the corresponding delivery request.';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_10
+BEFORE INSERT ON cancelled_requests
+FOR EACH ROW EXECUTE FUNCTION trigger_fn_10();
+
+-- Q11 For each delivery request, first return_leg's ID should be 1, second return_leg's ID should be 2, and so on
+CREATE OR REPLACE FUNCTION trigger_fn_11() RETURNS TRIGGER
+AS $$
+DECLARE
+    largest_leg_id INTEGER;
+BEGIN
+    SELECT COALESCE(MAX(r.leg_id), 0) INTO largest_leg_id FROM return_legs r WHERE request_id = NEW.request_id;
+    IF NEW.leg_id = largest_leg_id + 1 THEN
+        RETURN NEW;
+    ELSE
+        -- RAISE EXCEPTION 'Leg ID is invalid. Leg IDs must be consecutive integers starting from 1.'
+        RETURN NULL;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_11
+BEFORE INSERT ON return_legs
+FOR EACH ROW EXECUTE FUNCTION trigger_fn_11();
+
+-- Q12 For a delivery request, the first return leg cannot be inserted if... total of 3 conditions
+CREATE OR REPLACE FUNCTION trigger_fn_12() RETURNS TRIGGER
+AS $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM legs L WHERE L.request_id = NEW.request_id) THEN 
+        -- RAISE EXCEPTION 'There should be an existing leg for the delivery request.';
+        RETURN NULL;
+    ELSIF NEW.start_time < (SELECT MAX(end_time) FROM legs L WHERE L.request_id = NEW.request) THEN 
+        -- RAISE EXCEPTION 'The end_time of the last existing leg should be after the start_time of the return leg.';
+        RETURN NULL;
+    ELSIF EXISTS (SELECT 1 FROM cancelled_requests C WHERE C.id = NEW.request_id) THEN
+        -- Nested condition...
+        IF NEW.start_time < (SELECT cancel_time FROM cancelled_requests C WHERE C.id = NEW.request_id) THEN 
+            -- RAISE EXCEPTION 'The start_time of the return_leg should be after the cancel_time of the request.';
+            RETURN NULL;
+        END IF;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_12
+BEFORE INSERT ON return_legs 
+FOR EACH ROW EXECUTE FUNCTION trigger_fn_12();
+
+-- Q13 For each delivery return request, there can be at most three unsuccessful return deliveries
+CREATE OR REPLACE FUNCTION trigger_fn_13() RETURNS TRIGGER
+AS $$
+DECLARE
+    unsuccessful_return_deliveries_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO unsuccessful_return_deliveries_count FROM unsuccessful_return_deliveries u WHERE NEW.request_id = u.request_id;
+
+    IF (unsuccessful_deliveries_count >= 3) THEN
+        -- RAISE EXCEPTION 'Cannot have more than three unsuccessful return deliveries in a delivery request!';
+        RETURN NULL;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_13
+BEFORE INSERT ON unsuccessful_deliveries
+FOR EACH ROW EXECUTE FUNCTION trigger_fn_13();
+
+
+-- Q14 Unsuccessful return deliveries 
+CREATE OR REPLACE FUNCTION trigger_fn_14() RETURNS TRIGGER 
+AS $$
+DECLARE 
+	leg_timestamp TIMESTAMP;
+BEGIN
+	SELECT start_time INTO leg_timestamp
+	FROM return_legs L
+	WHERE L.id = NEW.id;
+
+	IF NEW.attempt_time <= delivery_timestamp THEN
+		-- RAISE EXCEPTION 'Delivery timestamp should have a value greater than leg timestamp.';
+		RETURN NULL;
+	END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_14 
+BEFORE INSERT ON unsuccessful_return_deliveries
+FOR EACH ROW EXECUTE FUNCTION trigger_fn_14();
--- Assignment 2/seed.sql
+++ Assignment 2/seed.sql
@@ -1,58 +1,87 @@
-INSERT INTO facilities (address, postal) 
-VALUES
-    ('123 Main St', 'A1B 2C3'),
-    ('456 Oak Ave', 'D4E 5F6'),
-    ('789 Elm Blvd', 'G7H 8I9'),
-    ('1011 Maple Rd', 'J1K 2L3'),
-    ('1213 Pine St', 'M4N 5O6');
+-- customers
+INSERT INTO customers (name, gender, mobile) VALUES
+('John Doe', 'male', '123-456-7890'),
+('Jane Smith', 'female', '234-567-8901'),
+('David Johnson', 'male', '345-678-9012'),
+('Mary Williams', 'female', '456-789-0123'),
+('Michael Brown', 'male', '567-890-1234'),
+('Emily Davis', 'female', '678-901-2345'),
+('Christopher Garcia', 'male', '789-012-3456'),
+('Emma Martinez', 'female', '890-123-4567'),
+('Matthew Rodriguez', 'male', '901-234-5678'),
+('Olivia Wilson', 'female', '012-345-6789');
 
-INSERT INTO legs (request_id, leg_id, handler_id, start_time, end_time, destination_facility)
-VALUES 
-    (1, 1, 101, '2023-04-01 10:00:00', '2023-04-01 12:00:00', 1),
-    (1, 2, 101, '2023-04-01 13:00:00', '2023-04-01 15:00:00', 2),
-    (2, 1, 102, '2023-04-02 09:00:00', '2023-04-02 11:00:00', 3),
-    (2, 2, 102, '2023-04-02 12:00:00', '2023-04-02 14:00:00', 4),
-    (2, 3, 102, '2023-04-02 15:00:00', '2023-04-02 17:00:00', 5);
+-- employees
+INSERT INTO employees (name, gender, dob, title, salary) VALUES
+('Peter Parker', 'male', '1990-05-10', 'Manager', 60000),
+('Clark Kent', 'male', '1995-03-15', 'Supervisor', 45000),
+('Diana Prince', 'female', '1985-06-20', 'Coordinator', 48000),
+('Bruce Wayne', 'male', '1980-02-25', 'Executive', 65000),
+('Natasha Romanoff', 'female', '1990-07-30', 'Driver', 35000),
+('Tony Stark', 'male', '1985-10-05', 'Driver', 35000),
+('Steve Rogers', 'male', '1995-12-10', 'Driver', 35000),
+('Wanda Maximoff', 'female', '2000-01-15', 'Driver', 35000),
+('Sam Wilson', 'male', '1990-03-20', 'Driver', 35000),
+('Carol Danvers', 'female', '1985-05-25', 'Driver', 35000);
 
-INSERT INTO delivery_requests (
-	customer_id, evaluater_id, status, pickup_addr, pickup_postal, recipient_name, recipient_addr, recipient_postal, submission_time, pickup_date, num_days_needed, price
-) 
-VALUES (
-	1, 2, 'accepted', 'mai address', 'A1B 2C3', 'John Doe', 'wan xuan address', 'D4E 5F6', '2023-04-01 10:00:00', '2023-04-05', 3, 50.00
-),
-(
-	2, 3, 'accepted', 'kai address', 'G7H 8I9', 'Jane Smith', 'qian address', 'J1K 2L3', '2023-04-02 09:00:00', '2023-04-04', 2, 75.00
-);
+-- delivery_staff
+INSERT INTO delivery_staff (id) VALUES
+(5), (6), (7), (8), (9), (10);
 
-INSERT INTO customers VALUES (1, "mai", 'male', '00000000'), (2, 'kai', 'male', '01010101');
+-- facilities
+INSERT INTO facilities (address, postal) VALUES
+('123 Main St', '10001'),
+('456 Oak St', '10002'),
+('789 Maple St', '10003'),
+('111 Elm St', '10004'),
+('222 Pine St', '10005'),
+('333 Cedar St', '10006'),
+('444 Birch St', '10007'),
+('555 Walnut St', '10008'),
+('666 Cherry St', '10009'),
+('777 Chestnut St', '10010');
 
-INSERT INTO employees VALUES ()
+INSERT INTO delivery_requests (id, customer_id, evaluater_id, status, pickup_addr, pickup_postal, recipient_name, recipient_addr, recipient_postal, submission_time, pickup_date, num_days_needed, price) VALUES
+(11, 3, 3, 'completed', '15 Maple St', '10002', 'John Doe', '25 Oak St', '10005', '2023-03-10 14:20:00', '2023-03-12', 2, 15.00),
+(12, 4, 2, 'completed', '88 Elm St', '10003', 'Jane Smith', '77 Pine St', '10008', '2023-03-15 10:30:00', '2023-03-16', 1, 10.00),
+(13, 5, 1, 'completed', '99 Birch St', '10004', 'Alice Johnson', '22 Cedar St', '10007', '2023-03-20 16:45:00', '2023-03-22', 2, 15.00),
+(14, 6, 3, 'completed', '44 Willow St', '10005', 'Bob Miller', '66 Palm St', '10009', '2023-03-25 09:15:00', '2023-03-26', 1, 10.00),
+(15, 7, 2, 'completed', '77 Cherry St', '10006', 'Carol Jackson', '11 Maple St', '10010', '2023-03-30 12:50:00', '2023-04-01', 2, 15.00);
 
-(
-	id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL,
-	gender gender_type NOT NULL,
-	dob DATE NOT NULL,
-	title TEXT NOT NULL,
-	salary NUMERIC NOT NULL,
-	CHECK (salary >= 0)
-);
+INSERT INTO accepted_requests (id, card_number, payment_time, monitor_id) VALUES
+(11, '1234567812345678', '2023-03-10 14:25:00', 5),
+(12, '2345678923456789', '2023-03-15 10:35:00', 6),
+(13, '3456789034567890', '2023-03-20 16:50:00', 7),
+(14, '4567890145678901', '2023-03-25 09:20:00', 8),
+(15, '5678901256789012', '2023-03-30 12:55:00', 9);
+
+INSERT INTO legs (request_id, leg_id, handler_id, start_time, end_time, destination_facility) VALUES
+(11, 1, 2, '2023-03-12 10:00:00', '2023-03-12 10:30:00', 1),
+(11, 2, 2, '2023-03-12 11:00:00', '2023-03-12 11:45:00', 2),
+(12, 1, 3, '2023-03-16 09:00:00', '2023-03-16 09:45:00', 1),
+(12, 2, 3, '2023-03-16 10:15:00', '2023-03-16 11:00:00', 2),
+(13, 1, 4, '2023-03-22 08:00:00', '2023-03-22 08:45:00', 1),
+(13, 2, 4, '2023-03-22 09:15:00', '2023-03-22 10:00:00', 2),
+(14, 1, 5, '2023-03-26 13:00:00', '2023-03-26 13:45:00', 1),
+(14, 2, 5, '2023-03-26 14:15:00', '2023-03-26 15:00:00', 2),
+(15, 1, 6, '2023-04-01 11:00:00', '2023-04-01 11:45:00', 1),
+(15, 2, 6, '2023-04-01 12:15:00', '2023-04-01 13:00:00', 2);
+
+INSERT INTO unsuccessful_deliveries (request_id, reason) VALUES
+(16, 'Recipient not available'),
+(17, 'Incorrect address'),
+(18, 'Damaged package');
+
+INSERT INTO return_legs (request_id, leg_id, handler_id, start_time, end_time, origin_facility) VALUES
+(16, 1, 2, '2023-03-15 12:00:00', '2023-03-15 12:45:00', 2),
+(16, 2, 2, '2023-03-15 13:15:00', '2023-03-15 14:00:00', 1),
+(17, 1, 3, '2023-03-20 10:00:00', '2023-03-20 10:45:00', 2),
+(17, 2, 3, '2023-03-20 11:15:00', '2023-03-20 12:00:00', 1),
+(18, 1, 4, '2023-03-25 14:00:00', '2023-03-25 14:45:00', 2),
+(18, 2, 4, '2023-03-25 15:15:00', '2023-03-25 16:00:00', 1);
+
+INSERT INTO unsuccessful_return_deliveries (request_id, reason) VALUES
+(19, 'Incorrect return address'),
+(20, 'Return facility closed');
 
 
-CALL submit_request(
-    1,
-    2,
-    pickup_addr TEXT,
-    pickup_postal TEXT,
-    recipient_name TEXT,
-    recipient_addr TEXT,
-    recipient_postal TEXT,
-    submission_time TIMESTAMP,
-    package_num INTEGER,
-    reported_height INTEGER [],
-    reported_width INTEGER [],
-    reported_depth INTEGER [],
-    reported_weight INTEGER [],
-    content TEXT [],
-    estimated_value NUMERIC []
-);
