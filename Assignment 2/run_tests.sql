491e16172385fac6c04d1092aee83e29d8321439
ferusel <wheatgrass.mc@gmail.com>
2023-04-10 17:45:55 +0800

    AHHHHHHHHHHHHHHHHHHHHHHHHH

--- Assignment 2/run_tests.sql
+++ Assignment 2/run_tests.sql
@@ -4,6 +4,6 @@
 -- \i test/trigger_3.sql;
 -- \i test/trigger_5.sql;
 -- \i test/trigger_9.sql;
-\i test/trigger_11.sql;
-\i test/trigger_12.sql;
-\i test/trigger_13.sql;
+-- \i test/trigger_11.sql;
+-- \i test/trigger_12.sql;
+-- \i test/trigger_13.sql;
